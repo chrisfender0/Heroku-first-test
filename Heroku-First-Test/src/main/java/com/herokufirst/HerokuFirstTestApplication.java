d1c94555074a510dd646e7da573363a0a2ebb42e
Chris <chrisfender0@gmail.com>
2020-12-17 16:17:31 -0800

    init commit

---
+++ Heroku-First-Test/src/main/java/com/herokufirst/HerokuFirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.herokufirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuFirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuFirstTestApplication.class, args);
+	}
+
+}
